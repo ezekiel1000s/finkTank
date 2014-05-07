9c95dcccf84580c61c21481fa602b946c49799f6
ezekiel1000s <ezekiel1000s@gmail.com>
2014-05-07 15:50:10 +0200

    Me first finktank commit. Hello Guvnarr!

---
+++ finktank.py
@@ -0,0 +1,20 @@
+# this program is designed to suck in mental diarrhea that comes up unconsciously. It is supposed to put all this mental bullshit into order and
+# perhaps help make some sense of it.
+# it could also free your mind of what bugs it.
+# the ideas could be gathered into a whole
+# it works from the CLI and is invoked as fast as possible, so that it captures the thought the moment it comes up
+# perhaps therewould be tagging possibilities
+# consider adding a database backend to this shit
+# take good together with the evil
+# take shit together with the gold
+# timestamp
+# diary-like
+
+
+
+
+
+
+
+
+
